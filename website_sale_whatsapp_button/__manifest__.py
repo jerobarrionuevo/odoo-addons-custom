96b6e4b28e63185f1a6064c6e586a7f953c13744
Jero Barrionuevo <jerobarrionuevo@gmail.com>
2026-02-09 13:25:59 +0000

    prueba de inserción de botón para finalizar la compra por whatsapp

---
+++ website_sale_whatsapp_button/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Website Sale WhatsApp Button",
+    "version": "16.0.1.0.0",
+    "category": "Website",
+    "summary": "Add WhatsApp button to cart",
+    "depends": [
+        "website_sale"
+    ],
+    "data": [
+        "views/cart_whatsapp_button.xml"
+    ],
+    "installable": True,
+    "application": False,
+}
