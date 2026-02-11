35e371137a794cf6b97c5df088c2ad4a5761d68e
Jero Barrionuevo <jerobarrionuevo@gmail.com>
2026-02-11 18:25:36 +0000

    Corrección de versión en manifest

--- website_sale_whatsapp_button/__manifest__.py
+++ website_sale_whatsapp_button/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Website Sale WhatsApp Button",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.2.0",
     "category": "Website",
     "summary": "Add WhatsApp button to cart",
     "depends": [
